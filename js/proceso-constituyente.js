2a6202ac7fb1a3ed721d65cacbdaca1c50c85b1b
ジョセフ·ダミアン·ガリード <damian.garrido.munoz@gmail.com>
2016-04-22 10:05:27 -0300

    upgrade - add angularjs - remove _data and add data

---
+++ js/proceso-constituyente.js
@@ -0,0 +1,21 @@
+var app = angular.module('webApp', [], function($interpolateProvider) {
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
+});
+
+app.controller('homepageController', function($scope,$http){
+  var $scope.orgs = [];
+  var $scope.orgInfo = [];
+
+  $http.get('/data/orgs.json')
+    .success(function (data) {
+      $scope.orgs = data
+    })
+    .error(function (){
+      $scope.messages = { response: false, message: 'no org loaded' }
+    });
+
+  $scope.getOrgInfo = function ($org) {
+    $scope.orgInfo = $org.petitions
+  }
+})
